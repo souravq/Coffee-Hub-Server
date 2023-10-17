0fb1970e4bf51b9ca24a010f52d41363ec201240
souravq <souravbera515@gmail.com>
2023-10-17 13:02:41 +0530

    Project setup done

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = 5000;
+
+app.get('/',(req,res)=>{
+    res.send("Hello World");
+})
+
+app.listen(PORT,()=>{
+    console.log(`App is listening on PORT ${PORT} `)
+})
